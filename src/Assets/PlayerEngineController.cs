790a099fabb6688689ce7c5010141a169c536949
Jarl Erik Schmidt <github@jarlerik.com>
2015-06-13 23:24:58 +0200

    Added game

---
+++ src/Assets/PlayerEngineController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEngineController : MonoBehaviour
+{
+    public KeyCode ignitionKey = KeyCode.W;
+    
+    private ParticleSystem _flare;
+    private Rigidbody2D _rigidbody2D;
+
+	// Use this for initialization
+	void Start ()
+	{
+        _flare = gameObject.GetComponentInChildren<ParticleSystem>();
+	    _rigidbody2D = gameObject.GetComponentInParent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKey(ignitionKey))
+	    {
+	        _flare.enableEmission = true;
+            _rigidbody2D.AddForceAtPosition(Vector2.up * 1f, gameObject.transform.localPosition);
+	    }
+	    else
+	    {
+	        _flare.enableEmission = false;
+	    } 
+	}
+}
---
+++ src/Assets/ShoveBastard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ShoveBastard : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Random.Range(0, 100) > 95)
+        {
+	    
+	        var left = Random.Range(0, 1) == 0;
+	        var force = Random.Range(0.02f, 0.03f);
+
+            _rigidbody2D.AddForceAtPosition((left ? Vector2.left  : Vector2.right) * force, gameObject.transform.TransformPoint(0, 1, 0));
+
+            Debug.Log("PUSH!");
+	    }
+
+
+	}
+}
